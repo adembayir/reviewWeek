cb792bf2bf7a72baedb80bd3528ac8072ec5c6ca
Adem BAYIR <adembyr@hotmail.com>
2022-06-01 16:04:23 +0300

    UpperCase - LowerCase difference is corrected.

--- src/test/java/com/cydeo/tests/reviewWeek/Task01.java
+++ src/test/java/com/cydeo/tests/reviewWeek/Task01.java
@@ -12,10 +12,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.security.Key;
-import java.util.Arrays;
-
-public class task01 {
+public class Task01 {
 
     public static Double totalPrice() {
         try {
